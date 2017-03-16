857ea121facef6e2174a2a687c410d68610347d3
Fabricio Oliveira <fabricioque@gmail.com>
2017-03-16 01:12:16 -0300

    rubocop

--- spec/dummy/db/migrate/20170309220100_create_foos.rb
+++ spec/dummy/db/migrate/20170309220100_create_foos.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class CreateFoos < ActiveRecord::Migration
   def change
     create_table :foos do |t|
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,4 +1,4 @@
-# encoding: UTF-8
+# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,33 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315232801) do
-
-  create_table "bars", force: :cascade do |t|
-    t.string   "other"
-    t.string   "uuid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_170_315_232_801) do
+  create_table 'bars', force: :cascade do |t|
+    t.string   'other'
+    t.string   'uuid'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_index "bars", ["uuid"], name: "index_bars_on_uuid", unique: true
+  add_index 'bars', ['uuid'], name: 'index_bars_on_uuid', unique: true
 
-  create_table "foos", force: :cascade do |t|
-    t.string   "other"
-    t.string   "uuid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'foos', force: :cascade do |t|
+    t.string   'other'
+    t.string   'uuid'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_index "foos", ["uuid"], name: "index_foos_on_uuid", unique: true
+  add_index 'foos', ['uuid'], name: 'index_foos_on_uuid', unique: true
 
-  create_table "pepes", force: :cascade do |t|
-    t.string   "other"
-    t.string   "uuid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'pepes', force: :cascade do |t|
+    t.string   'other'
+    t.string   'uuid'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_index "pepes", ["uuid"], name: "index_pepes_on_uuid", unique: true
-
+  add_index 'pepes', ['uuid'], name: 'index_pepes_on_uuid', unique: true
 end
