a14c4503103d66a08d38d300cb8da9716c9d1053
Fabricio Oliveira <fabricioque@gmail.com>
2017-03-16 00:48:48 -0300

    fix test

--- spec/integration_test/app/helper/tp_optimistic_unique/unique_spec.rb
+++ spec/integration_test/app/helper/tp_optimistic_unique/unique_spec.rb
@@ -4,38 +4,42 @@ require 'rails_helper'
 RSpec.describe TPRecordOptimistic, type: :request do
   describe 'create two model same uuid' do
     context 'When unique is activated' do
-      before do
-        post foo_index_path
-        post foo_index_path
+      context 'create' do
+        before do
+          post foo_index_path
+          post foo_index_path
+        end
+
+        it 'Does return status_code: 400' do
+          expect(response.status).to eq 400
+        end
       end
 
-      it 'Does return status_code: 400' do
-        expect(response.status).to eq 400
-      end
-    end
+      context 'create!' do
+        before do
+          post pepe_index_path
+          post pepe_index_path
+        end
 
-    context 'When unique is activated and create' do
-      before do
-        post pepe_index_path
-        post pepe_index_path
-      end
-
-      it 'Does return status_code: 409' do
-        expect(response.status).to eq 409
+        it 'Does return status_code: 409' do
+          expect(response.status).to eq 409
+        end
       end
     end
 
     context 'When unique is not activated' do
-      before do
-        post bar_index_path
-      end
+      context 'create' do
+        before do
+          post bar_index_path
+        end
 
-      let(:uuid) do
-        SecureRandom.uuid
-      end
+        let(:uuid) do
+          SecureRandom.uuid
+        end
 
-      it 'Does return status_code: 400' do
-        expect { post bar_index_path }.to raise_error(ActiveRecord::RecordNotUnique)
+        it 'Does return status_code: 400' do
+          expect { post bar_index_path }.to raise_error(ActiveRecord::RecordNotUnique)
+        end
       end
     end
   end
